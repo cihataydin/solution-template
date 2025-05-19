a1d508b14af72bc83288465923b36b7651909567
cihataydin <cihataydin.tr@gmail.com>
2025-05-19 16:45:22 +0300

    feat(41): enables database query logging with serilog
    - adds sample controler for test

--- src/Infra/Data/DataContext.cs
+++ src/Infra/Data/DataContext.cs
@@ -1,10 +1,19 @@
 using Domain.Entities;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Serilog;
+
 
 namespace Infra.Data;
 public class DataContext : DbContext
 {
     public DataContext(DbContextOptions<DataContext> opts) : base(opts) { }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.LogTo(Log.Logger.Information);
+        base.OnConfiguring(optionsBuilder);
+    }
 
     public virtual DbSet<SampleEntity> Products => Set<SampleEntity>();
 }
